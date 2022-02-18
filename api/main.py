96fe11d53a31932be8ef13549a5eb59d989ab1ba
Ben Rucker <brucker117@gmail.com>
2022-02-18 17:00:03 -0500

    Add db dependency

--- api/main.py
+++ api/main.py
@@ -59,74 +59,73 @@ def read_message(msg_id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/message/{msg_id}", response_model=schemas.Message, tags=['Messages'], dependencies=[token])
-def add_message(msg_id: int, message: schemas.MessageCreate):
+def add_message(msg_id: int, message: schemas.MessageCreate, db: Session = Depends(get_db)):
     return {"msg_id": msg_id, "message": message}
 
 
 @app.patch("/message/{msg_id}", response_model=schemas.Message, tags=['Messages'], dependencies=[token])
-def update_message(msg_id: int, message: schemas.MessageCreate):
+def update_message(msg_id: int, message: schemas.MessageCreate, db: Session = Depends(get_db)):
     # follow partial schema docs, search for 'patch'
-    # 
     return message
 
 
 @app.delete("/message/{msg_id}", response_model=schemas.Message, tags=['Messages'], dependencies=[token])
-def delete_message(msg_id: int):
+def delete_message(msg_id: int, db: Session = Depends(get_db)):
     return {"msg_id": msg_id}
 
 
 @app.put("/pin/{msg_id}", response_model=schemas.Message, tags=["Messages"], dependencies=[token])
-def pin_message(msg_id):
+def pin_message(msg_id, db: Session = Depends(get_db)):
     # use crud.update_message
     return f'pinned {msg_id}'
 
 
 @app.get("/user/{user_id}", response_model=schemas.User, tags=["Users"])
-def get_user(user_id: int):
+def get_user(user_id: int, db: Session = Depends(get_db)):
     return {"user_id": user_id}
 
 
 @app.put("/user/{user_id}", response_model=schemas.User, tags=["Users"], dependencies=[token])
-def add_user(user_id: int, data: schemas.User):
+def add_user(user_id: int, data: schemas.User, db: Session = Depends(get_db)):
     return {"user_id": user_id, "data": data}
 
 
 @app.patch("/user/{user_id}", response_model=schemas.User, tags=["Users"], dependencies=[token])
-def update_user(user_id: int, data: schemas.User):
+def update_user(user_id: int, data: schemas.User, db: Session = Depends(get_db)):
     # TODO partial
     return {"user_id": user_id, "data": data}
 
 
 @app.get("/user/{user_id}/score", tags=["Users"])
-def get_user_score(user_id: int):
+def get_user_score(user_id: int, db: Session = Depends(get_db)):
     return {"user_id": user_id, "score": 500}
 
 
 @app.post("/reaction", tags=['Reactions'], dependencies=[token])
-def add_reaction(reaction: schemas.Reaction):
+def add_reaction(reaction: schemas.Reaction, db: Session = Depends(get_db)):
     return {"msg_id": reaction.msg_id, "user_id": reaction.msg_id, "reaction": reaction.emoji}
 
 
 @app.delete('/reaction', tags=['Reactions'], dependencies=[token])
-def delete_reaction(reaction: schemas.Reaction):
+def delete_reaction(reaction: schemas.Reaction, db: Session = Depends(get_db)):
     return {"msg_id": reaction.msg_id, "user_id": reaction.msg_id, "reaction": reaction.emoji}
 
 
 @app.put("/channel/{chan_id}", tags=['Channels'], dependencies=[token])
-def add_channel(chan_id: int, channel: schemas.Channel):
+def add_channel(chan_id: int, channel: schemas.Channel, db: Session = Depends(get_db)):
     return {"name": channel.name}
 
 
 @app.get("/channel/{chan_id}", tags=['Channels'])
-def get_channel(chan_id: int):
+def get_channel(chan_id: int, db: Session = Depends(get_db)):
     return chan_id
 
 
 @app.patch("/channel/{chan_id}", tags=['Channels'], dependencies=[token])
-def update_channel(chan_id: int, channel: schemas.Channel):
+def update_channel(chan_id: int, channel: schemas.Channel, db: Session = Depends(get_db)):
     return {"name": channel.name}
 
 
 @app.post("/voice_event", tags=["Events"], dependencies=[token])
-def add_voice_event(event: schemas.VoiceEvent):
+def add_voice_event(event: schemas.VoiceEvent, db: Session = Depends(get_db)):
     return {"user": event.user_id, "action": event.type, "time": event.timestamp}
